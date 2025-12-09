aed600472bee751ae2fa172f0de13a0c85d0b446
extaazza <timshinnikitos2110@gmail.com>
2025-12-09 16:45:45 +0300

    Add files via upload

---
+++ OAIP/LR-9-10/pack 1-5.pas
@@ -0,0 +1,107 @@
+﻿program z1;
+
+const
+  max_size = 100; // Константа для максимального размера массива
+
+type
+  TArray = array[1..max_size] of integer; // Определим тип массива
+
+var
+  A, B: TArray; // Глобальные переменные для хранения массивов
+  sizeA, sizeB: integer; // Глобальные переменные для размеров массивов
+
+// Процедура для ввода массива
+procedure InputArray(var Arr: TArray; var size: integer); // Формальные параметры, передача по ссылке
+var
+  i: integer; // Локальная переменная
+begin
+  write('Введите размер массива: ');
+  readln(size); // Фактический параметр size, передача по ссылке
+  for i := 1 to size do // Фактический параметр size, передача по значению
+  begin
+    write('Введите элемент ', i, ': ');
+    readln(Arr[i]); // Фактический параметр Arr, передача по ссылке
+  end;
+end;
+
+// Процедура для печати массива
+procedure PrintArray(const Arr: TArray; const size: integer); // Формальные параметры, передача по значению
+var
+  i: integer; // Локальная переменная
+begin
+  for i := 1 to size do // Фактический параметр size, передача по значению
+    write(Arr[i], ' '); // Фактический параметр Arr, передача по значению
+  writeln;
+end;
+
+// Процедура для замены максимального элемента нулем для массива A
+procedure ReplaceMaxWithZero(var Arr: TArray; const size: integer); // Формальные параметры, передача по ссылке и по значению
+var
+  i, MaxIdx: Integer; // Локальные переменные
+  DelenieNaFive: Boolean; // Локальная переменная для проверки кратного пяти
+begin
+  DelenieNaFive := False; // Локальная переменная для проверки кратного пяти
+  // Поиск первого кратного пяти элемента
+  for i := 1 to size do // Фактический параметр size, передача по значению
+  begin
+    if (Arr[i] mod 5 = 0) then // Фактический параметр Arr, передача по ссылке
+    begin
+      DelenieNaFive := True;
+      break;
+    end;
+  end;
+  
+  // Если найден кратный пяти элемент, найти максимальный и заменить его на 0
+  if DelenieNaFive then
+  begin
+    MaxIdx := 1; // Индекс максимального элемента
+    for i := 2 to size do // Фактический параметр size, передача по значению
+    begin
+      if Arr[i] > Arr[MaxIdx] then // Фактический параметр Arr, передача по ссылке
+        MaxIdx := i; // Обновляем индекс максимального элемента
+    end;
+    Arr[MaxIdx] := 0; // Заменяем максимальный элемент на 0
+  end;
+end;
+
+// Процедура для умножения на 2 всех элементов после минимального элемента для массива B
+procedure MultiplyAfterMin(var Arr: TArray; const size: integer); // Формальные параметры, передача по ссылке и по значению
+var
+  MinIdx, i: integer; // Локальные переменные
+begin
+  // Находим индекс минимального элемента
+  MinIdx := 1;
+  for i := 2 to size do // Фактический параметр size, передача по значению
+  begin
+    if Arr[i] < Arr[MinIdx] then // Фактический параметр Arr, передача по ссылке
+      MinIdx := i; // Обновляем индекс минимального элемента
+  end;
+  
+  // Умножаем все элементы после минимального на 2
+  for i := MinIdx + 1 to size do // Фактический параметр size, передача по значению
+  begin
+    Arr[i] := Arr[i] * 2; // Увеличиваем элемент в два раза, фактический параметр Arr, передача по ссылке
+  end;
+end;
+
+begin
+  // Ввод данных для массива A
+  InputArray(A, SizeA); // Фактические параметры, передача по ссылке
+  // Ввод данных для массива B
+  InputArray(B, SizeB); // Фактические параметры, перед
+ writeln('Исходный массив A:');
+  PrintArray(A, SizeA);
+  
+  writeln('Исходный массив B:');
+  PrintArray(B, SizeB);
+  
+  // Обработка массивов
+  ReplaceMaxWithZero(A, SizeA);
+  MultiplyAfterMin(B, SizeB);
+  
+  writeln('Измененный массив A:');
+  PrintArray(A, SizeA);
+  
+  writeln('Измененный массив B:');
+  PrintArray(B, SizeB);
+end.
---
+++ OAIP/LR-9-10/pack 2-5.pas
@@ -0,0 +1,95 @@
+﻿program z2;
+
+const
+  N = 8; // Размер двумерного массива
+
+type
+  TArray2D = array[1..N, 1..N] of integer; // Объявление типа двумерного массива
+  TArray1D = array[1..N] of integer; // Объявление типа одномерного массива
+
+// Процедура для заполнения двумерного массива случайными числами
+procedure FillArray(var Arr: TArray2D);
+var
+  i, j: integer;
+begin
+  randomize; // Инициализация генератора случайных чисел
+  for i := 1 to N do
+    for j := 1 to N do
+      Arr[i, j] := random(100); // Заполнение массива случайными числами от 0 до 99
+end;
+
+// Процедура для проверки наличия единственного максимума в строке
+function FirstMax(const Arr: TArray2D; Row: integer): boolean;
+var
+  i, MaxValue, Count: integer;
+begin
+  MaxValue := Arr[Row, 1];
+  Count := 1;
+
+  // Находим максимальное значение и считаем количество его вхождений
+  for i := 2 to N do
+  begin
+    if Arr[Row, i] > MaxValue then
+    begin
+      MaxValue := Arr[Row, i];
+      Count := 1; // Сбрасываем счетчик при нахождении нового максимума
+    end
+    else if Arr[Row, i] = MaxValue then
+      Count := Count + 1; // Увеличиваем счетчик при нахождении равного максимума
+  end;
+
+  // Возвращаем true, если максимум уникальный
+  FirstMax := (Count = 1);
+end;
+
+// Процедура для формирования одномерного массива на основе двумерного
+procedure CreateResultArray(const Arr: TArray2D; var result: TArray1D);
+var
+  i: integer;
+begin
+  for i := 1 to N do
+  begin
+    if FirstMax(Arr, i) then
+      result[i] := 1
+    else
+      result[i] := -1;
+  end;
+end;
+
+// Процедура для вывода двумерного массива
+procedure PrintArray2D(const Arr: TArray2D);
+var
+  i, j: integer;
+begin
+  for i := 1 to N do
+  begin
+    for j := 1 to N do
+      write(Arr[i, j]:4); // Вывод каждого элемента с отступом
+    writeln; // Переход к следующей строке
+  end;
+end;
+
+// Процедура для вывода одномерного массива
+procedure PrintArray1D(const Arr: TArray1D);
+var
+  i: integer;
+begin
+  for i := 1 to N do
+    write(Arr[i]:4);
+  writeln; // Переход на новую строку
+end;
+
+var
+  Array2D: TArray2D; // Объявление двумерного массива
+  ResultArray: TArray1D; // Объявление одномерного массива
+
+begin
+  FillArray(Array2D); // Заполнение двумерного массива случайными числами
+
+  writeln('Двумерный массив:');
+  PrintArray2D(Array2D); // Вывод двумерного массива
+
+  CreateResultArray(Array2D, ResultArray); // Формирование одномерного массива на основе результата
+  writeln('Результирующий одномерный массив:');
+  PrintArray1D(ResultArray); // Вывод одномерного массива
+end.
